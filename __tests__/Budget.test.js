cd484556ce42d22d6a771dc8bd9cf0ac9fd7aa79
johnrobertmcc <john.robert.mcc@gmail.com>
2022-01-04 09:44:53 -0500

    added enzyme packages - not working

---
+++ __tests__/Budget.test.js
@@ -0,0 +1,9 @@
+import Budget from '@/pages/budget/index'
+import React from 'react'
+import {mount} from 'enzyme'
+
+test('The page should be mounted', () => {
+  const wrapper = mount(<Budget />)
+
+  // expect(wrapper)
+})
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,21 @@
+const {defaults} = require('jest-config')
 module.exports = {
   moduleNameMapper: {
     '^@/components/(.*)$': '<rootDir>/components/$1',
     '^@/functions/(.*)$': '<rootDir>/functions/$1',
     '^@/styles/(.*)$': '<rootDir>/styles/$1',
     '^@/pages/(.*)$': '<rootDir>/pages/$1'
-  }
+  },
+  transform: {
+    ...defaults.transform,
+    '^.+\\.js?$': 'babel-jest'
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/setup-tests.js'],
+  transformIgnorePatterns: [
+    ...defaults.transformIgnorePatterns,
+    '/node_modules/(?!my-package)(.*)'
+  ],
+  moduleNameMapper: {...defaults.moduleNameMapper},
+  verbose: true
 }
